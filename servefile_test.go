e7f8f88491452c3847a97a8910cbb857279180a1
xuewenG <gexuewen@outlook.com>
2024-12-29 17:21:29 +0800

    fix: typo

--- servefile_test.go
+++ servefile_test.go
@@ -3,7 +3,7 @@ package traefik_plugin_servefile_test
 import (
 	"testing"
 
-	traefik_plugin_blockheader "github.com/xuewenG/traefik-plugin-servefile"
+	traefik_plugin_servefile "github.com/xuewenG/traefik-plugin-servefile"
 )
 
 type HeaderConfig struct {
@@ -13,7 +13,7 @@ type HeaderConfig struct {
 
 func TestCreateConfig(t *testing.T) {
 	t.Run("create config", func(t *testing.T) {
-		config := traefik_plugin_blockheader.CreateConfig()
+		config := traefik_plugin_servefile.CreateConfig()
 		if config == nil {
 			t.Fatal("create config failed")
 		}
